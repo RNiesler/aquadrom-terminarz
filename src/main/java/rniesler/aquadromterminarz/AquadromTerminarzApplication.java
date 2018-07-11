5a77f2b2e0fb2727cd65647e1dfc47888e88cfc6
Rafal Niesler <rniesler@gmail.com>
2018-07-11 21:44:13 +0200

    Generated spring boot project

---
+++ src/main/java/rniesler/aquadromterminarz/AquadromTerminarzApplication.java
@@ -0,0 +1,12 @@
+package rniesler.aquadromterminarz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AquadromTerminarzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AquadromTerminarzApplication.class, args);
+	}
+}
