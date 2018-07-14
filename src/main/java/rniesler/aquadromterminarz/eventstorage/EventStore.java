b141521e73a454aaffef0fe89bd6f28872c41a2e
Rafal Niesler <rniesler@gmail.com>
2018-07-14 08:41:24 +0200

    Make the update of aggregate version atomic

--- src/main/java/rniesler/aquadromterminarz/eventstorage/EventStore.java
+++ src/main/java/rniesler/aquadromterminarz/eventstorage/EventStore.java
@@ -1,11 +1,18 @@
 package rniesler.aquadromterminarz.eventstorage;
 
+import com.mongodb.client.result.UpdateResult;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.OptimisticLockingFailureException;
+import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
+import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
+
+import static org.springframework.data.mongodb.core.query.Criteria.where;
+import static org.springframework.data.mongodb.core.query.Query.query;
 
 @Service
 @Slf4j
@@ -13,11 +20,13 @@ public class EventStore { //TODO make reactive
     private final EventLogRepository eventLogRepository;
     private final EventLogHandler eventLogHandler;
     private final AggregateRepository aggregateRepository;
+    private final ReactiveMongoTemplate mongoTemplate;
 
-    public EventStore(EventLogRepository eventLogRepository, EventLogHandler eventLogHandler, AggregateRepository aggregateRepository) {
+    public EventStore(EventLogRepository eventLogRepository, EventLogHandler eventLogHandler, AggregateRepository aggregateRepository, ReactiveMongoTemplate mongoTemplate) {
         this.eventLogRepository = eventLogRepository;
         this.eventLogHandler = eventLogHandler;
         this.aggregateRepository = aggregateRepository;
+        this.mongoTemplate = mongoTemplate;
     }
 
     public Mono<UUID> newAggregate(String aggregateType, EventLog eventLog) {
@@ -36,15 +45,19 @@ public class EventStore { //TODO make reactive
     }
 
     public Mono<EventLog> saveEvent(EventLog eventLog) {
-        return aggregateRepository.findById(eventLog.getAggregateId()) // TODO make it atomic!!!!!
-                .flatMap(aggregate -> {
-                    if (aggregate.getVersion() != eventLog.getVersion()) {
-                        throw new RuntimeException("Optimistic Locking failure"); //TODO exception
+        // atomic query and increment
+        Mono<UpdateResult> updateResultMono = mongoTemplate.updateFirst(
+                query(where("aggregateId").is(eventLog.getAggregateId()).and("version").is(eventLog.getVersion())),
+                new Update().inc("version", 1),
+                Aggregate.class);
+        return updateResultMono
+                .map(updateResult -> {
+                    if (updateResult.getModifiedCount() == 0) {
+                        throw new OptimisticLockingFailureException("Aggregate optimistic locking failure");
                     }
-                    aggregate.setVersion(aggregate.getVersion() + 1);
-                    return aggregateRepository.save(aggregate);
+                    return updateResult;
                 })
-                .flatMap(aggregate -> eventLogRepository.save(eventLog))
+                .flatMap(updateResult -> eventLogRepository.save(eventLog))
                 .flatMap(savedEventLog -> eventLogHandler.handle(savedEventLog).thenReturn(savedEventLog));
     }
 }
